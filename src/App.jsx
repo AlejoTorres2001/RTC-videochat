a9b49c49b871d570b44e2693ea9c78f22396076c
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-06-13 20:19:19 -0300

    eslint config

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-function App () {
-  return <div className="App">
-    <h1 className="bg-red-400">Hello world! !</h1>
-  </div>
+import { useEffect, useState } from 'react'
+import io from 'socket.io-client'
+
+const socket = io.connect(
+  import.meta.env.MODE === 'development'
+    ? 'http://localhost:3000'
+    : 'https://rtc-videochat-backend.herokuapp.com'
+)
+function App() {
+  const [stream, setStream] = useState('')
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then((stream) => {
+        setStream(stream)
+      })
+  }, [])
+  return (
+    <div className="App">
+      <h1 className="bg-red-400">Hello world! !</h1>
+    </div>
+  )
 }
 
 export default App
