1a1e65264761c255e765fa2aaa5d0037846c9534
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-06-14 12:04:29 -0300

    testing CI/CD

--- src/App.jsx
+++ src/App.jsx
@@ -174,7 +174,7 @@ function App() {
           {receivingCall && !callAccepted
             ? (
             <div className="caller">
-              <h1>{name} is calling...</h1>
+              <h1>{name} is calling... </h1>
               <Button variant="contained" color="primary" onClick={answerCall}>
                 Answer
               </Button>
