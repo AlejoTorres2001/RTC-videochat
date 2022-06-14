df0e8d9568c49c6ae46ace18b30ec0c6f50a10bf
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-06-14 11:38:06 -0300

    formatted

--- src/server.js
+++ src/server.js
@@ -1,35 +1,32 @@
-// make and express server
 const express = require('express')
+const http = require('http')
 const app = express()
-const server = require('http').createServer(app)
-const cors = require('cors')
+const server = http.createServer(app)
 const io = require('socket.io')(server, {
   cors: {
-    origin: process.env.NODE_ENV === 'production' ? 'https://rtc-videochat-client.herokuapp.com' : 'http://localhost:3001',
-    methods: ['GET,POST,PUT,DELETE']
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST']
   }
 })
-app.use(cors())
-const PORT = process.env.PORT || 3000
 
 io.on('connection', (socket) => {
-  socket.emmit('me', socket.id)
-  socket.on('disconnet', () => {
-    socket.broadcast.emmit('callEnded')
+  socket.emit('me', socket.id)
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('callEnded')
   })
-  socket.on('callUser', ({ userToCall, signalData, from, name }) => {
-    io.to(userToCall).emmit('callUser', { signal: signalData, from, name })
+
+  socket.on('callUser', (data) => {
+    io.to(data.userToCall).emit('callUser', {
+      signal: data.signalData,
+      from: data.from,
+      name: data.name
+    })
   })
+
   socket.on('answerCall', (data) => {
-    io.to(data.to).emmit('callAccepted', data.signal)
+    io.to(data.to).emit('callAccepted', data.signal)
   })
 })
-app.get('/health', (req, res) => {
-  res.send('ok')
-})
-app.get('/', (req, res) => {
-  res.send('server ready')
-})
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT} `)
-})
+
+server.listen(5000, () => console.log('server is running on port 5000'))
